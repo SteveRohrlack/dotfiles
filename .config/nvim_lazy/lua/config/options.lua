787b215a9c175b30d36cf7a0f05729ed1a10e099
SteveRohrlack <elmo.dd@gmail.com>
2024-12-09 19:59:09 +0000

    feat: port vim options from old nvchad setup

--- .config/nvim_lazy/lua/config/options.lua
+++ .config/nvim_lazy/lua/config/options.lua
@@ -1,3 +1,32 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- vim.cmd('au TextYankPost * silent! lua vim.highlight.on_yank()')
+-- vim.wo.relativenumber = true
+
+vim.fn.sign_define("DapBreakpoint", { text = "⛔️" })
+vim.fn.sign_define("DapBreakpointRejected", { text = "🚫" })
+vim.fn.sign_define("DapStopped", { text = "📌" })
+vim.fn.sign_define("DapLogPoint", { text = "⛳︎" })
+vim.fn.sign_define("DapBreakpointCondition", { text = "🔴" })
+
+local set = vim.opt
+set.ignorecase = true
+set.smartcase = true
+
+-- set.colorcolumn = "120"
+set.textwidth = 120
+
+-- use treesitter as default foldmethod
+-- set.foldmethod = "expr"
+-- set.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.opt.foldlevel = 99
+
+vim.filetype.add({
+  pattern = {
+    [".*.jenkinsfile"] = "groovy",
+    ["Jenkinsfile..*"] = "groovy",
+    ["Jenkinsfile"] = "groovy",
+  },
+})

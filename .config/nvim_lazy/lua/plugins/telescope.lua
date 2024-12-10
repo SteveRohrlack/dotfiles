0371ab08afcddc27b55f519ff8b44257225cfe78
SteveRohrlack <elmo.dd@gmail.com>
2024-12-10 18:48:33 +0000

    feat: find marks via telescope

--- .config/nvim_lazy/lua/plugins/telescope.lua
+++ .config/nvim_lazy/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
     -- disable the keymap to grep files
     { "<leader>/", false },
     { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "grep files" },
+    { "<leader>fm", "<cmd>Telescope marks<cr>", desc = "marks" },
     {
       "<leader>ff",
       "<cmd>Telescope find_files follow=true no_ignore=true hidden=true<cr>",

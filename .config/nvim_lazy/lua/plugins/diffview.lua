f279f31a9488d427b8430332419f7d6db8f60755
SteveRohrlack <elmo.dd@gmail.com>
2024-12-10 18:48:48 +0000

    feat: toggle diffview with a single keymap

--- .config/nvim_lazy/lua/plugins/diffview.lua
+++ .config/nvim_lazy/lua/plugins/diffview.lua
@@ -2,7 +2,17 @@ return {
   "sindrets/diffview.nvim",
   cmd = { "DiffviewOpen", "DiffviewToggleFiles" },
   keys = {
-    { "<leader>gD", "<cmd>DiffviewOpen<cr>", desc = "diff view" },
-    { "<leader>gH", "% <cmd>DiffviewFileHistory<cr>", desc = "diff view" },
+    {
+      "<leader>gD",
+      function()
+        if next(require("diffview.lib").views) == nil then
+          vim.cmd("DiffviewOpen")
+        else
+          vim.cmd("DiffviewClose")
+        end
+      end,
+      desc = "diff view",
+    },
+    { "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "diff view" },
   },
 }

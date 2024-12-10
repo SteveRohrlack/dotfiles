bd06eaefc9851a06b90bad919729164fb3842463
SteveRohrlack <elmo.dd@gmail.com>
2024-12-10 18:47:33 +0000

    fix: disable custom dap icons for now, lazyvim has beautiful icons, too

--- .config/nvim_lazy/lua/config/options.lua
+++ .config/nvim_lazy/lua/config/options.lua
@@ -5,11 +5,11 @@
 -- vim.cmd('au TextYankPost * silent! lua vim.highlight.on_yank()')
 -- vim.wo.relativenumber = true
 
-vim.fn.sign_define("DapBreakpoint", { text = "⛔️" })
-vim.fn.sign_define("DapBreakpointRejected", { text = "🚫" })
-vim.fn.sign_define("DapStopped", { text = "📌" })
-vim.fn.sign_define("DapLogPoint", { text = "⛳︎" })
-vim.fn.sign_define("DapBreakpointCondition", { text = "🔴" })
+-- vim.fn.sign_define("DapBreakpoint", { text = "⛔️" })
+-- vim.fn.sign_define("DapBreakpointRejected", { text = "🚫" })
+-- vim.fn.sign_define("DapStopped", { text = "📌" })
+-- vim.fn.sign_define("DapLogPoint", { text = "⛳︎" })
+-- vim.fn.sign_define("DapBreakpointCondition", { text = "🔴" })
 
 local set = vim.opt
 set.ignorecase = true
